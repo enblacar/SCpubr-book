4a83a3b59cf6a8048d2d32d8bcba3f983626b0cc
enblacar <e.blancocarmona@dkfz-heidelberg.de>
2022-11-03 11:10:34 +0100

    Transitioning website to Quarto instead of Bookdown.

--- _common.R
+++ _common.R
@@ -3,14 +3,14 @@ options(width = 60)
 
 # example chunk options set globally
 knitr::opts_chunk$set(
-    collapse = TRUE,
-    comment = "#>",
-    message = FALSE,
-    warning = FALSE,
-    fig.align = 'center',
-    fig.width = 6,
-    fig.height = 6,
-    out.width = "100%",
-    out.height = "100%",
-    dpi=300
-  )
+  collapse = TRUE,
+  comment = "#>",
+  message = FALSE,
+  warning = FALSE,
+  fig.align = 'center',
+  fig.width = 6,
+  fig.height = 6,
+  out.width = "100%",
+  out.height = "100%",
+  dpi=300
+)
