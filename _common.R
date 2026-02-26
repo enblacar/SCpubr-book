5a0b48eb35cc617a953b910934cac5baeea26ef6
enblacar <enblacar1996@gmail.com>
2026-02-26 18:32:55 +0100

    v3.0.1 update

--- _common.R
+++ _common.R
@@ -8,19 +8,18 @@ knitr::opts_chunk$set(
   message = FALSE,
   warning = FALSE,
   fig.align = 'center',
-  fig.width = 6,
-  fig.height = 6,
-  out.width = "100%",
-  out.height = "100%",
-  dpi=300
+  fig.width = 12,
+  fig.height = 12,
+  dpi = 300
 )
 
 # Load required libraries for all chapters
 suppressMessages(library(Seurat))
 suppressMessages(library(SCpubr))
+suppressMessages(library(ggplot2))
+suppressMessages(library(patchwork))
 
-# Helper: consistent plot theme for all SCpubr plots
-scpubr_theme <- function() {
-  ggplot2::theme_minimal(base_size = 12) +
-    ggplot2::theme(legend.position = "right")
-}
+# Load the sample dataset
+# ATRT single-nucleus RNA-seq data from Blanco-Carmona et al. (2025)
+# DOI: 10.1093/neuonc/noaf179 | PMID: 40726147
+sample <- readRDS("/Users/eblanco/Desktop/Projects/CLUSTER_CHECK/ATRT_data/ATRT_RNA.rds")
